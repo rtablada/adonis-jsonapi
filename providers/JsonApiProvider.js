9b086eabbbbed1e5c96eac0272d59b2ab6343b32
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-31 10:07:47 -0500

    Update and share

--- providers/JsonApiProvider.js
+++ providers/JsonApiProvider.js
@@ -1,21 +1,33 @@
 'use strict';
 
-let fold;
+const ServiceProvider = require('adonis-fold').ServiceProvider;
+const JsonApiSerializer = require('jsonapi-serializer').Serializer;
 
-try {
-  fold = require('adonis-fold');
-} catch (_) {
-  // workaround when `npm link`'ed for development
-  const prequire = require('parent-require');
-  fold = prequire('adonis-fold');
+function setupSerializer(make, response) {
+  return function (serializerName, data, statusCode = 200) {
+    const helpers = make('Helpers');
+
+    const { type, serializer } = make(helpers.makeNameSpace('Http/Serializers', serializerName));
+
+    if (data.toJSON && typeof data.toJSON === 'function') {
+      data = data.toJSON();
+    }
+    const pluralizeType = Array.isArray(data);
+
+    const options = Object.assign({}, serializer, { pluralizeType });
+
+    const json = new JsonApiSerializer(type, options).serialize(data);
+
+    this.status(statusCode).json(json);
+  };
 }
-
-const ServiceProvider = fold.ServiceProvider;
 
 class JsonApiProvider extends ServiceProvider {
 
   * register() {
+    const Response = this.app.use('Adonis/Src/Response');
 
+    // Response.macro('jsonApi', setupSerializer(this.app.use));
   }
 
 }
