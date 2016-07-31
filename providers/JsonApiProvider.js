bd3c1cc927fabcafe6de17487cfcab149f1197db
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-31 09:54:25 -0500

    Use parent-require for local development

--- providers/JsonApiProvider.js
+++ providers/JsonApiProvider.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const ServiceProvider = require('adonis-fold').ServiceProvider;
+let fold;
+
+try {
+  fold = require('adonis-fold');
+} catch (_) {
+  // workaround when `npm link`'ed for development
+  const prequire = require('parent-require');
+  fold = prequire('adonis-fold');
+}
+
+const ServiceProvider = fold.ServiceProvider;
 
 class JsonApiProvider extends ServiceProvider {
 
